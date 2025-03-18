1cca38211b078d5dd2dae30509a702192d081077
NamithaTS <namithasrinivasa@gmail.com>
2025-03-18 12:28:04 +0530

    Add files via upload

---
+++ Day 12/Namespaces and Scope/task.py
@@ -0,0 +1,23 @@
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+
+def loc():
+    l=9
+    print(l)
+loc()
+
+p=34
+def gob():
+
+
+    print(p)
+gob()
